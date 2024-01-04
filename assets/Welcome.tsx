73650eaceaa81f877111d3f3f698239f9a0dc4c2
Jeffrey Schwartz <jefftschwartz@gmail.com>
2024-01-04 11:28:35 -0500

    Addresses issue #14: Link to documentation is wrong.

--- assets/Welcome.tsx
+++ assets/Welcome.tsx
@@ -5,7 +5,7 @@ export const Welcome = function() {
             <p>Your project has been created.</p>
             <h2>Where To Go From Here</h2>
             <ul>
-                <li><p>If you are new to fusion.ssg, please read the <a href="https://fusionssg.com/docs">docs</a>.</p></li>
+                <li><p>If you are new to fusion.ssg, please read the <a href="https://fusionssg.com/docs/v1">docs</a>.</p></li>
                 <li><p>Please give us a <a href="https://github.com/4awpawz/fusion.ssg">✰</a> and <a href="https://github.com/4awpawz/fusion.ssg">watch</a> for notifications.</p></li>
                 <li> <p><b><i>Go get creative with fusion.ssg!</i></b></p></li>
             </ul>
